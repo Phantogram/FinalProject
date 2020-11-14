ae5c82286004621b383384092d45858366d5a014
Phantogram <seattle_elliot@yahoo.com>
2020-11-14 14:59:27 -0800

    val push

---
+++ config.py
@@ -0,0 +1,4 @@
+api_key = '1XIr7GLUUo4mY/OkCY04/wrVz99EyaDw+aCs69eWBwVuKw8wMdJT3h6FUZKt2UWDYQ9yg4Nbo6Ln4WyJegRz1Q=='
+
+url = 'https://ussouthcentral.services.azureml.net/workspaces/0860328401bc467ba9848b4ffdd94923/services/f7a7e34f0c7c4c2084160f25422776a3/execute?api-version=2.0&details=true'
+
