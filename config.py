247a02bdeebf9e88f4b77d4f0c44454901ad4c4a
Val Popovich <seattle_elliot@yahoo.com>
2020-11-14 15:22:42 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-api_key = '1XIr7GLUUo4mY/OkCY04/wrVz99EyaDw+aCs69eWBwVuKw8wMdJT3h6FUZKt2UWDYQ9yg4Nbo6Ln4WyJegRz1Q=='
+api_key = 'api_key'
 
-url = 'https://ussouthcentral.services.azureml.net/workspaces/0860328401bc467ba9848b4ffdd94923/services/f7a7e34f0c7c4c2084160f25422776a3/execute?api-version=2.0&details=true'
-
+url = 'url'
